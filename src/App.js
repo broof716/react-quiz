5057f18b074146205d2ec4d6176984ee9713c748
Bryan Roof <broof716@gmail.com>
2020-05-11 16:29:05 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,167 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, {useReducer} from 'react';
+import Progress from './components/Progress';
+import Question from './components/Question';
+import Answers from './components/Answers';
+import QuizContext from './context/QuizContext';
+
+
+import {
+    SET_ANSWERS,
+    SET_CURRENT_QUESTION,
+    SET_CURRENT_ANSWER,
+    SET_ERROR,
+    SET_SHOW_RESULTS,
+    RESET_QUIZ,
+} from './reducers/types.js';
+import quizReducer from './reducers/QuizReducer';
+
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+    const questions = [
+        {
+            id: 1,
+            question: 'Who is the current homerun leader in baseball?',
+            answer_a:
+                'Babe Ruth',
+            answer_b: 'Barry Bonds',
+            answer_c:
+                "Hank Aaron",
+            answer_d: 'Alex Rodriguez',
+            correct_answer: 'b',
+        },
+        {
+            id: 2,
+            question: 'Who won the 2001 World Series?',
+            answer_a: 'New York Yankees',
+            answer_b: 'Arizona Diamondbacks',
+            answer_c: 'Boston Redsoxs',
+            answer_d: 'Los Angeles Dodgers ',
+            correct_answer: 'b',
+        },
+        {
+            id: 3,
+            question: 'Who owns the strikeout record in a single season?',
+            answer_a: 'Randy Johnson',
+            answer_b: 'Nolan Ryan',
+            answer_c: 'Matt Kilroy',
+            answer_d: 'Curt Schilling',
+            correct_answer: 'c',
+        },
+        {
+          id: 4,
+          question: 'Who has won the most world series?',
+          answer_a: 'Boston Redsoxs',
+          answer_b: 'St. Louis Cardinals',
+          answer_c: 'Pittsburg Pirates',
+          answer_d: 'New York Yankees',
+          correct_answer: 'd',
+      },
+    ];
+
+    const initialState = {
+        questions,
+        currentQuestion: 0,
+        currentAnswer: '',
+        answers: [],
+        showResults: false,
+        error: '',
+    };
+
+    const [state, dispatch] = useReducer(quizReducer, initialState);
+    const {currentQuestion, currentAnswer, answers, showResults, error} = state;
+
+    const question = questions[currentQuestion];
+
+    const renderError = () => {
+        if (!error) {
+            return;
+        }
+
+        return <div className="error">{error}</div>;
+    };
+
+    const renderResultMark = (question, answer) => {
+        if (question.correct_answer === answer.answer) {
+            return <span className="correct">Correct</span>;
+        }
+
+        return <span className="failed">Failed</span>;
+    };
+
+    const renderResultsData = () => {
+        return answers.map(answer => {
+            const question = questions.find(
+                question => question.id === answer.questionId
+            );
+
+            return (
+                <div key={question.id}>
+                    {question.question} - {renderResultMark(question, answer)}
+                </div>
+            );
+        });
+    };
+
+    const restart = () => {
+        dispatch({type: RESET_QUIZ});
+    };
+
+    const next = () => {
+        const answer = {questionId: question.id, answer: currentAnswer};
+
+        if (!currentAnswer) {
+            dispatch({type: SET_ERROR, error: 'Please select an option'});
+            return;
+        }
+
+        answers.push(answer);
+        dispatch({type: SET_ANSWERS, answers});
+        dispatch({type: SET_CURRENT_ANSWER, currentAnswer: ''});
+
+        if (currentQuestion + 1 < questions.length) {
+            dispatch({
+                type: SET_CURRENT_QUESTION,
+                currentQuestion: currentQuestion + 1,
+            });
+            return;
+        }
+
+        dispatch({type: SET_SHOW_RESULTS, showResults: true});
+    };
+
+    if (showResults) {
+        return (
+            <div className="container results">
+                <h2>Results</h2>
+                <ul>{renderResultsData()}</ul>
+                <button className="btn btn-primary" onClick={restart}>
+                    Restart
+                </button>
+            </div>
+        );
+    } else {
+        return (
+            <QuizContext.Provider value={{state, dispatch}}>
+                <div className="container">
+                    <Progress
+                        total={questions.length}
+                        current={currentQuestion + 1}
+                    />
+                    <Question />
+                    {renderError()}
+                    <Answers />
+                    <button className="btn btn-primary" onClick={next}>
+                        Confirm and Continue
+                    </button>
+                </div>
+            </QuizContext.Provider>
+        );
+    }
 }
 
 export default App;
